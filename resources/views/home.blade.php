58864f3a6acc080801ccb81aeb1ab79296320a87
shipotech <shipotech@gmail.com>
2019-02-20 02:39:57 -0400

    1- Bugs fixed 2- New style for the home page, better layout
    -- Let's started with the likes logic :D --

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,16 +4,89 @@
 <div class="container mt-lg-5 py-5">
     <div class="row justify-content-center">
         <div class="col-md-4 order-md-2 mb-5 pr-lg-5 pl-lg-5">
-            <!-- Card -->
-                <div class="card promoting-card mb-2">
-                    <!-- Card content -->
-                    <div class="card-body d-flex flex-row py-2 align-items-center">
-                        <!-- Avatar -->
-                        <img src="{{ asset('storage/users/' . Auth::user()->image) }}" class="rounded-circle mr-3" height="50px" width="50px" alt="avatar">
-                        <!-- Content -->
-                        <div class="mdb-color-text">
-                            <!-- Nick -->
-                            <h4 class="card-title font-weight-bold mb-1 h5-responsive">{{ Auth::user()->name . ' ' . Auth::user()->surname}}</h4>
+            {{--<!-- Card -->--}}
+                {{--<div class="card promoting-card mb-2">--}}
+                    {{--<!-- Card content -->--}}
+                    {{--<div class="card-body d-flex flex-row py-2 align-items-center">--}}
+                        {{--<!-- Avatar -->--}}
+                        {{--<img src="{{ asset('storage/users/' . Auth::user()->image) }}" class="rounded-circle mr-3" height="50px" width="50px" alt="avatar">--}}
+                        {{--<!-- Content -->--}}
+                        {{--<div class="mdb-color-text">--}}
+                            {{--<!-- Nick -->--}}
+                            {{--<h4 class="card-title font-weight-bold mb-1 h5-responsive">{{ Auth::user()->name . ' ' . Auth::user()->surname}}</h4>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+
+                    {{--<!-- Card content -->--}}
+                    {{--<div class="card-body m-0 py-0">--}}
+                        {{--<div class="alert alert-danger fade text-danger d-none text-sm" role="alert" id="error">--}}
+                            {{--<strong>Whoops!</strong> <span>The file must be an image (jpeg, png)</span>--}}
+                        {{--</div>--}}
+
+                        {{--@if($errors->any())--}}
+                            {{--<div class="alert alert-danger fade show text-danger text-sm" role="alert">--}}
+                                {{--<strong>Whoops!</strong>--}}
+                                    {{--@foreach($errors->all() as $message)--}}
+                                        {{--<span>{{ $message }}</span>--}}
+                                    {{--@endforeach--}}
+                            {{--</div>--}}
+                        {{--@endif--}}
+
+                        {{--@if(session('message'))--}}
+                            {{--<div class="alert alert-success alert-dismissible fade show text-sm" role="alert">--}}
+                                {{--<strong>Success!</strong> {{ session('message') }}.--}}
+                                {{--<button type="button" class="close" data-dismiss="alert" aria-label="Close">--}}
+                                    {{--<span aria-hidden="true">&times;</span>--}}
+                                {{--</button>--}}
+                            {{--</div>--}}
+                        {{--@endif--}}
+
+
+                        {{--<div class="mx-auto">--}}
+                            {{--<div class="view overlay overflow-hidden">--}}
+                                {{--<a title="Upload your photo" href="javascript:changeUpload()">--}}
+                                    {{--<img src="{{ asset('img/img2-escala.png') }}" alt="upload picture" class="img-fluid upload-preview w-100">--}}
+                                    {{--<div class="mask flex-center text-white rgba-black-strong overflow-hidden">--}}
+                                        {{--<i class="fa fa-camera fa-2x"></i>--}}
+                                    {{--</div>--}}
+                                {{--</a>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                        {{----}}
+                        {{--<div class="form-group shadow-textarea w-100">--}}
+                            {{--<form action="{{ route('image.store') }}" method="post" enctype="multipart/form-data" id="upload-form">--}}
+                                {{--@csrf--}}
+                                {{--<input type="file" name="upload" id="upload-image" style="display: none" required/>--}}
+                                {{--<label for="description" class="sr-only-focusable"></label>--}}
+                                {{--<textarea name="description" class="form-control z-depth-1" id="description" rows="2" placeholder="Write something here..." required></textarea>--}}
+                                {{--<button type="submit" class="btn indigo accent-4 border-0 hoverable float-right  mx-0 my-4 white-text">--}}
+                                    {{--<i class="fas fa-upload" aria-hidden="true"></i> Upload--}}
+                                {{--</button>--}}
+                            {{--</form>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+
+                {{--</div>--}}
+                {{--<!-- Card -->--}}
+
+
+            <!-- Card NEW -->
+                <article class="card news-card mb-5">
+
+                    <!-- Heading-->
+                    <div class="card-body w-100 py-2">
+                        <div class="content d-flex">
+                            <div class="row w-100 m-0 p-0">
+                                <div class="col-12 p-0">
+                                    <img src="@if(Auth::user()->image) {{ asset('storage/users/' . Auth::user()->image) }} @else {{ asset('img/img3-escala.png') }} @endif"
+                                         class="rounded-circle avatar-img d-block d-sm-inline float-none float-sm-left mx-auto mx-sm-0 mr-sm-3" alt="avatar">
+                                    <div class="col text-center text-sm-left">
+                                        <p class="font-weight-bold text-dark m-0">
+                                            {{ Auth::user()->nick }}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
@@ -26,9 +99,9 @@
                         @if($errors->any())
                             <div class="alert alert-danger fade show text-danger text-sm" role="alert">
                                 <strong>Whoops!</strong>
-                                    @foreach($errors->all() as $message)
-                                        <span>{{ $message }}</span>
-                                    @endforeach
+                                @foreach($errors->all() as $message)
+                                    <span>{{ $message }}</span>
+                                @endforeach
                             </div>
                         @endif
 
@@ -41,32 +114,34 @@
                             </div>
                         @endif
 
-
                         <div class="mx-auto">
                             <div class="view overlay overflow-hidden">
                                 <a title="Upload your photo" href="javascript:changeUpload()">
-                                    <img src="{{ asset('img/img2-escala.png') }}" alt="upload picture" class="img-fluid upload-preview w-100">
+                                    <img src="{{ asset('img/img3-escala.png') }}" alt="upload picture" class="img-fluid upload-preview w-100">
                                     <div class="mask flex-center text-white rgba-black-strong overflow-hidden">
                                         <i class="fa fa-camera fa-2x"></i>
                                     </div>
                                 </a>
                             </div>
-
                         </div>
+
                         <div class="form-group shadow-textarea w-100">
                             <form action="{{ route('image.store') }}" method="post" enctype="multipart/form-data" id="upload-form">
                                 @csrf
                                 <input type="file" name="upload" id="upload-image" style="display: none" required/>
                                 <label for="description" class="sr-only-focusable"></label>
                                 <textarea name="description" class="form-control z-depth-1" id="description" rows="2" placeholder="Write something here..." required></textarea>
-                                <button type="submit" class="btn indigo accent-4 border-0 hoverable float-right  mx-0 my-4 white-text">
-                                    <i class="fas fa-upload" aria-hidden="true"></i> Upload
-                                </button>
+                                <div class="row">
+                                    <div class="col">
+                                        <button type="submit" class="btn indigo accent-4 border-0 hoverable mt-4 m-0 w-100 white-text">
+                                            <i class="fas fa-upload" aria-hidden="true"></i> Upload
+                                        </button>
+                                    </div>
+                                </div>
                             </form>
                         </div>
                     </div>
-
-                </div>
+                </article>
                 <!-- Card -->
         </div>
 
@@ -77,23 +152,23 @@
                     <article class="card news-card mb-5">
 
                         <!-- Heading-->
-                        <header class="card-body w-100 px-0 py-2">
-                            <div class="content">
-                                <div class="row w-100 m-0 p-0">
-                                    <div class="col-12 text-center d-flex flex-row">
-                                        <img src="@if($image->user->image) {{ asset('storage/users/' . $image->user->image) }} @else {{ asset('img/img3-escala.png') }} @endif" class="rounded-circle avatar-img" alt="avatar">
-                                        <div class="col-md d-flex flex-column align-items-start">
-                                            <p class="font-weight-bold m-0">
+                            <header class="card-body w-100 py-2">
+                                <div class="content d-flex">
+                                    <div class="row w-100 m-0 p-0">
+                                        <div class="col-12 p-0">
+                                            <img src="@if($image->user->image) {{ asset('storage/users/' . $image->user->image) }} @else {{ asset('img/img3-escala.png') }} @endif"
+                                                 class="rounded-circle avatar-img d-block d-sm-inline float-none float-sm-left mx-auto mx-sm-0 mr-sm-3" alt="avatar">
+                                            <div class="col text-center text-sm-left">
+                                            <p class="font-weight-bold text-dark m-0">
                                                 {{ $image->user->nick }}
                                             </p>
-                                            <span class="text-sm text-muted font-weight-normal mt-2">
-                                        <i class="fas fa-clock"></i> {{ $image->created_at->diffForHumans() }}
-                                    </span>
+
+                                        <div class="text-sm text-muted font-weight-normal mt-1"><i class="fas fa-clock"></i> {{ $image->created_at->diffForHumans() }}</div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </header>
+                            </header>
 
                         <!-- Card image -->
                         <div class="view overlay">
@@ -107,7 +182,7 @@
                             </a>
                         </div>
 
-                        <div class="card-body pb-0 mdb-color-text">
+                        <div class="card-body pb-0 pt-3 mdb-color-text">
                             <div class="row p-0 m-0">
                                 <div class="col px-0">
                                     <a href="#" class="pr-2 text-muted icons likes">
@@ -127,7 +202,7 @@
                             </div>
                         </div>
                         <!-- Card content -->
-                        <div class="card-body px-3 pb-0 pt-2 mdb-color-text">
+                        <div class="card-body pb-0 pt-2 mdb-color-text">
                             <div class="collapse-content pb-3 pt-0">
                                 <!-- Text -->
                                 <p class="card-text collapse" id="collapseContent">
